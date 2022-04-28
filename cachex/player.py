c57d9e1b2e461a27519f7151d83ee28d1a4d873d
stobincouzen <sebastian.tobincouzens@student.unimelb.edu.au>
2022-04-29 02:01:07 +1000

    changed module from team_name to tenuOS, added .DS_Store to .gitignore, switched player.py in cachex to import player.py from /tenuOS/

--- cachex/player.py
+++ cachex/player.py
@@ -1,31 +1,5 @@
-class Player:
-    def __init__(self, player, n):
-        """
-        Called once at the beginning of a game to initialise this player.
-        Set up an internal representation of the game state.
+import sys
+sys.path.append(1, '~/Repos/Assignments/ai_part_2/team_name/')
 
-        The parameter player is the string "red" if your player will
-        play as Red, or the string "blue" if your player will play
-        as Blue.
-        """
-
-    def action(self):
-        """
-        Called at the beginning of your turn. Based on the current state
-        of the game, select an action to play.
-        """
-        return
-
-    def turn(self, player, action):
-        """
-        Called at the end of each player's turn to inform this player of
-        their chosen action. Update your internal representation of the
-        game state based on this. The parameter action is the chosen
-        action itself.
-
-        Note: At the end of your player's turn, the action parameter is
-        the same as what your player returned from the action method
-        above. However, the referee has validated it at this point.
-        """
-        return
+import player
 
